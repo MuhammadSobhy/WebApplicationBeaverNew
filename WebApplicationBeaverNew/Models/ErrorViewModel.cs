54b85c39204be7765a5358aeaf2739873ed8b4a3
muhammad sobhy <muhammad.sobhy.ahmed@gmail.com>
2020-02-25 07:33:05 +0000

    Add project files.

---
+++ WebApplicationBeaverNew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationBeaverNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
